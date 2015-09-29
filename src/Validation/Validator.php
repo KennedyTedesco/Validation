95f7ba3b38c77df0561c1794a0be51fac7dcdc4c
Ricardo Gobbo <ricardo@clicknow.com.br>
2015-09-29 17:46:34 -0300

    Validar qualquer nova regra

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -8,90 +8,35 @@ use KennedyTedesco\Validation\Respect\Factory as RuleFactory;
 class Validator extends BaseValidator
 {
     /**
-     * All supported rules.
-     *
-     * @var array
-     */
-    private $_validRules = [];
-
-    /**
-     * Create a new Validator instance.
-     *
-     * @param  \Symfony\Component\Translation\TranslatorInterface  $translator
-     * @param  array  $data
-     * @param  array  $rules
-     * @param  array  $messages
-     * @param  array  $customAttributes
-     */
-    public function __construct(TranslatorInterface $translator, array $data, array $rules, array $messages = [], array $customAttributes = [])
-    {
-        parent::__construct($translator, $data, $rules, $messages, $customAttributes);
-
-        $this->_validRules = $this->getValidRules();
-    }
-
-    /**
-     * Handle dynamic calls to class methods.
-     *
-     * @param  string  $method
-     * @param  array   $parameters
-     * @return mixed
-     */
-    public function __call($method, $parameters)
-    {
-        $rule = lcfirst(substr($method, 8));
-
-        if (in_array($rule, $this->_validRules)) {
-            $args = $parameters[2];
-            $value = $parameters[1];
-            $ruleObject = RuleFactory::make($rule, $args);
-            return $ruleObject->validate($value);
-        }
-
-        return parent::__call($method, $parameters);
-    }
-
-    /**
-     * Get all supported rules from Respect.
-     *
-     * @return bool
-     */
-    protected function getValidRules()
-    {
-        $path = __DIR__ . '/Respect/Rules.php';
-
-        return array_unique(require $path, SORT_REGULAR);
-    }
-
-    /**
      * Validate a minimum age.
      *
-     * @param  string  $attribute
-     * @param  mixed   $value
-     * @param  array   $parameters
+     * @param string $attribute
+     * @param string $value
+     * @param array  $parameters
+     *
      * @return bool
      */
     public function validateMinimumAge($attribute, $value, $parameters)
     {
-        $parameter = (int) $parameters[0];
-        return RuleFactory::make('MinimumAge', array($parameter))->validate($value);
+        return RuleFactory::make('MinimumAge', [empty($parameters[0]) ? 18 : (int) $parameters[0], empty($parameters[1]) ? null : $parameters[1]])->validate($value);
     }
 
     /**
      * Validate if file exists.
      *
-     * @param  string  $attribute
-     * @param  mixed   $value
-     * @param  array   $parameters
+     * @param string $attribute
+     * @param string $value
+     * @param array  $parameters
+     *
      * @return bool
      */
     public function validateFileExists($attribute, $value, $parameters)
     {
-        return RuleFactory::make('exists', array())->validate($value);
+        return RuleFactory::make('exists', [])->validate($value);
     }
 
     /**
-     * Replace all place-holders for the MinimumAge rule.
+     * Replace all error message place-holders with actual values.
      *
      * @param  string  $message
      * @param  string  $attribute
@@ -99,64 +44,34 @@ class Validator extends BaseValidator
      * @param  array   $parameters
      * @return string
      */
-    protected function replaceMinimumAge($message, $attribute, $rule, $parameters)
+    protected function doReplacements($message, $attribute, $rule, $parameters)
     {
-        return str_replace(':age', $parameters[0], $message);
+        $message = parent::doReplacements($message, $attribute, $rule, $parameters);
+        $search = [];
+        foreach ($parameters as $key => $parameter) {
+            array_push($search, ':parameter'.$key);
+        }
+        return str_replace($search, $parameters, $message);
     }
 
     /**
-     * Replace all place-holders for the Contains rule.
+     * Handle dynamic calls to class methods.
      *
-     * @param  string  $message
-     * @param  string  $attribute
-     * @param  string  $rule
-     * @param  array   $parameters
-     * @return string
-     */
-    protected function replaceContains($message, $attribute, $rule, $parameters)
-    {
-        return str_replace(':value', $parameters[0], $message);
-    }
-
-    /**
-     * Replace all place-holders for the Charset rule.
+     * @param string $method
+     * @param array  $parameters
      *
-     * @param  string  $message
-     * @param  string  $attribute
-     * @param  string  $rule
-     * @param  array   $parameters
-     * @return string
+     * @return mixed
      */
-    protected function replaceCharset($message, $attribute, $rule, $parameters)
+    public function __call($method, $parameters)
     {
-        return str_replace(':charset', $parameters[0], $message);
-    }
-
-    /**
-     * Replace all place-holders for the EndsWith rule.
-     *
-     * @param  string  $message
-     * @param  string  $attribute
-     * @param  string  $rule
-     * @param  array   $parameters
-     * @return string
-     */
-    protected function replaceEndsWith($message, $attribute, $rule, $parameters)
-    {
-        return str_replace(':value', $parameters[0], $message);
-    }
-
-    /**
-     * Replace all place-holders for the Multiple rule.
-     *
-     * @param  string  $message
-     * @param  string  $attribute
-     * @param  string  $rule
-     * @param  array   $parameters
-     * @return string
-     */
-    protected function replaceMultiple($message, $attribute, $rule, $parameters)
-    {
-        return str_replace(':value', $parameters[0], $message);
+        try {
+            $rule = lcfirst(substr($method, 8));
+            $args = $parameters[2];
+            $value = $parameters[1];
+            $ruleObject = RuleFactory::make($rule, $args);
+            return $ruleObject->validate($value);
+        } catch (\Exception $e) {
+            return parent::__call($method, $parameters);
+        }
     }
 }
